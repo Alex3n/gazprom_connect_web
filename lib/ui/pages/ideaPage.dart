aa0a780df4824fa8b5f3d4cddc276bbe7589f693
kirillnepomiluev <yallghgh789>
2020-06-20 05:36:06 +0300

    добавление актуальных отделов для идей

--- lib/ui/pages/ideaPage.dart
+++ lib/ui/pages/ideaPage.dart
@@ -16,7 +16,22 @@ class _IdeaPageState extends State<IdeaPage> {
   TextEditingController overviewProblemcontroller = TextEditingController();
   TextEditingController otdelcontroller = TextEditingController();
   TextEditingController decisionController = TextEditingController();
-  List <Map<String, dynamic>> listDeps =[];
+  List <Map<String, dynamic>> listDeps = [];
+
+  String getDepsNames() {
+    String names = "";
+    listDeps.forEach((element) {
+      names = names + element['title'] + " ";
+    });
+    return names;
+  }
+
+  void chooseDep(Map <String, dynamic> data) {
+    setState(() {
+      listDeps.add(data);
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -47,51 +62,77 @@ class _IdeaPageState extends State<IdeaPage> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
-                        Text('актуально для: всех'),
+                        Text('актуально для: ' + (listDeps.isEmpty
+                            ? "всех сотрудников"
+                            : getDepsNames())),
                         RaisedButton(
                           onPressed: () {
                             showDialog(
                                 context: context,
-                                builder: (_) => new Dialog(
-                                      child: Container(
-                                        width: 800,
-                                        child: Column(
-                                          children: <Widget>[
-                                            Padding(
-                                              padding: const EdgeInsets.all(8.0),
-                                              child: Text('Выберите отдел'),
-                                            ),
-                                            Expanded(
-                                              child: Container(child: StreamBuilder(
-                                                stream: store.collection("departments").onSnapshot,
-                                                builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                                                  if (snapshot.hasError)
-                                                    return new Text('Error: ${snapshot.error}');
-                                                  switch (snapshot.connectionState) {
-                                                    case ConnectionState.waiting: return new Text('Загрузка...');
-                                                    default:
-                                                      return new ListView.builder(
-                                                          shrinkWrap: true,
-                                                          physics: NeverScrollableScrollPhysics(),
-                                                          itemCount:  snapshot.data.docs.length,
-                                                          itemBuilder: (BuildContext ctx, int index) {
-                                                            return  GestureDetector(
-                                                              child: Padding(padding: EdgeInsets.all(10),
-                                                              child: Text(snapshot.data.docs.elementAt(index).data()['title'].toString(),),),
-                                                              onTap:() {
-
-                                                              },
-                                                            );
-                                                          }
-                                                      );
-                                                  }
-
-                                                },),),
-                                            )
-                                          ],
+                                builder: (_) =>
+                                new Dialog(
+                                  child: Container(
+                                    width: 800,
+                                    child: Column(
+                                      children: <Widget>[
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text('Выберите отдел'),
                                         ),
-                                      ),
-                                    ));
+                                        Expanded(
+                                          child: Container(child: StreamBuilder(
+                                            stream: store
+                                                .collection("departments")
+                                                .onSnapshot,
+                                            builder: (BuildContext context,
+                                                AsyncSnapshot<
+                                                    dynamic> snapshot) {
+                                              if (snapshot.hasError)
+                                                return new Text(
+                                                    'Error: ${snapshot.error}');
+                                              switch (snapshot
+                                                  .connectionState) {
+                                                case ConnectionState.waiting:
+                                                  return new Text(
+                                                      'Загрузка...');
+                                                default:
+                                                  return new ListView.builder(
+                                                      shrinkWrap: true,
+                                                      physics: NeverScrollableScrollPhysics(),
+                                                      itemCount: snapshot.data
+                                                          .docs.length,
+                                                      itemBuilder: (
+                                                          BuildContext ctx,
+                                                          int index) {
+                                                        return GestureDetector(
+                                                          child: Padding(
+                                                            padding: EdgeInsets
+                                                                .all(10),
+                                                            child: Text(
+                                                              snapshot.data.docs
+                                                                  .elementAt(
+                                                                  index)
+                                                                  .data()['title']
+                                                                  .toString(),),),
+                                                          onTap: () {
+                                                            chooseDep(
+                                                                snapshot.data
+                                                                    .docs
+                                                                    .elementAt(
+                                                                    index)
+                                                                    .data());
+                                                            Navigator.pop(context);
+                                                          },
+                                                        );
+                                                      }
+                                                  );
+                                              }
+                                            },),),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ));
                           },
                           child: Text('Выбрать отдел'),
                         )
@@ -119,16 +160,22 @@ class _IdeaPageState extends State<IdeaPage> {
       ),
     );
   }
+
   void clickWrite(BuildContext context) async {
     if (nameProblemcontroller.text.isNotEmpty) {
       Map<String, dynamic> newProduct = {
         'title': nameProblemcontroller.text,
         'description': overviewProblemcontroller.text.toString(),
-        'image': "controllerPhotoUrl.text,"
+        'image': "controllerPhotoUrl.text",
+        'departments' : listDeps
       };
 
       addNewDoc(context, "ideas", newProduct, whenDone: () {
         Navigator.pop(context);
       });
     }
-  }}
+  }
+}
+
+
+
