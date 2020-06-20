1db5fe72ea6b0eb3791eced63cb4bf2a31e2006c
voodoo420 <voodoo420@mail.ru>
2020-06-20 18:43:57 +0300

    добавление редактирования проектов в процессе

--- lib/ui/pages/AdminProject.dart
+++ lib/ui/pages/AdminProject.dart
@@ -3,6 +3,7 @@ import 'package:firebase/firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:gazpromconnectweb/func/mydb.dart';
+import 'package:gazpromconnectweb/themes/colors.dart';
 import 'package:gazpromconnectweb/ui/widgets/MyTexts.dart';
 import 'package:gazpromconnectweb/ui/widgets/myImageWidget.dart';
 import 'package:gazpromconnectweb/ui/widgets/storageUploadImageWidget.dart';
@@ -16,19 +17,33 @@ class AdminProjectsPage extends StatefulWidget {
 }
 
 class _AdminProjectsPageState extends State<AdminProjectsPage> {
+//  List<dynamic> ideas;
+//
+//  @override
+//  void initState() {
+//    store.collection("ideas").onSnapshot.listen((event) {
+//      event.docs.forEach((element) {
+//        ideas.add(element.data()['title']);
+//        print(element.data()['title']);
+//      });
+//    });
+//    super.initState();
+//  }
+
   @override
   Widget build(BuildContext context) {
     switch (adminContent) {
-      case AdminContent.editProject:
       case AdminContent.addProject:
-        return buildAddEmployee(context, data: currentData, id: currentId);
+        return buildAddProject();
+      case AdminContent.editProject:
+        return buildEditProject(context, data: currentData, id: currentId);
         break;
       default:
-        return buildEmployeesPage(context);
+        return buildProjectPage(context);
     }
   }
 
-  Widget buildEmployeesPage(BuildContext context) {
+  Widget buildProjectPage(BuildContext context) {
     return ListView(
       children: <Widget>[
         FlatButton(
@@ -128,12 +143,22 @@ class _AdminProjectsPageState extends State<AdminProjectsPage> {
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: new Text("состав команды: ${document.data()['employees'].toString()}",
+            child: new Text(
+              "состав команды: ${document.data()['employees'].toString()}",
               style: new TextStyle(
                   fontSize: 14.0,
                   color: Color(0xFF000000),
                   fontWeight: FontWeight.w300,
                   fontFamily: "Roboto"),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: RaisedButton(
+
+              color: mainappbarblue,
+              child: Text("Открыть чат проекта"),
+              onPressed: (){},
             ),
           ),
         ]));
@@ -150,9 +175,9 @@ class _AdminProjectsPageState extends State<AdminProjectsPage> {
 
   Widget buildMyCardWithPaddingNotOnTap(Widget child,
       {EdgeInsets padding = const EdgeInsets.fromLTRB(23.0, 12.0, 23.0, 10.0),
-        void Function() onTapFunc,
-        BuildContext context,
-        EdgeInsets margin = const EdgeInsets.all(4.0)}) {
+      void Function() onTapFunc,
+      BuildContext context,
+      EdgeInsets margin = const EdgeInsets.all(4.0)}) {
     return Padding(
       padding: margin,
       child: Card(
@@ -172,7 +197,7 @@ class _AdminProjectsPageState extends State<AdminProjectsPage> {
     );
   }
 
-  Widget buildAddEmployee(BuildContext context,
+  Widget buildEditProject(BuildContext context,
       {Map<String, dynamic> data, String id}) {
     final controllerPhotoUrl = TextEditingController();
     final controllerTitle = TextEditingController();
@@ -216,10 +241,10 @@ class _AdminProjectsPageState extends State<AdminProjectsPage> {
 
         updateDoc(context, newProduct, collection: "projects", doc: id,
             whenDone: () {
-              setState(() {
-                adminContent = AdminContent.projects;
-              });
-            });
+          setState(() {
+            adminContent = AdminContent.projects;
+          });
+        });
       }
     }
 
@@ -239,15 +264,16 @@ class _AdminProjectsPageState extends State<AdminProjectsPage> {
             height: MediaQuery.of(context).size.height / 4,
             child: UploadImageWidget(controllerUrl: controllerPhotoUrl)),
         buildTextForm(controllerTitle, hint: 'идея'),
-        buildTextForm(controllerDescription, hint: 'описание', label:'описание'),
-      _dropDownButton(),
+        buildTextForm(controllerDescription,
+            hint: 'описание', label: 'описание'),
+        _dropDownButton(),
 //        buildTextForm(controllerEmployees, hint: 'сотрудники', label: 'сотрудники'),
         buildTextForm(controllerPhotoUrl,
             hint: 'ссылка на фото', label: 'ссылка на фото'),
         new FlatButton(
             key: null,
             onPressed: () =>
-            data.isEmpty ? clickWrite(context) : clickUpdate(context),
+                data.isEmpty ? clickWrite(context) : clickUpdate(context),
             child: new Text(
               data.isEmpty ? "Сохранить" : "Сохранить изменения",
               style: new TextStyle(
@@ -258,16 +284,16 @@ class _AdminProjectsPageState extends State<AdminProjectsPage> {
             )),
         data.isNotEmpty
             ? new FlatButton(
-            key: null,
-            onPressed: () => clickDelete(context),
-            child: new Text(
-              "Удалить",
-              style: new TextStyle(
-                  fontSize: 12.0,
-                  color: const Color(0xFF000000),
-                  fontWeight: FontWeight.w200,
-                  fontFamily: "Roboto"),
-            ))
+                key: null,
+                onPressed: () => clickDelete(context),
+                child: new Text(
+                  "Удалить",
+                  style: new TextStyle(
+                      fontSize: 12.0,
+                      color: const Color(0xFF000000),
+                      fontWeight: FontWeight.w200,
+                      fontFamily: "Roboto"),
+                ))
             : Container()
       ],
     );
@@ -285,20 +311,76 @@ class _AdminProjectsPageState extends State<AdminProjectsPage> {
           value: solution,
           items: list
               .map((_value) => DropdownMenuItem(
-            child: Text(
-              _value,
-              style: TextStyle(
-                  fontSize: 12.0,
-                  color: const Color(0xFF000000),
-                  fontWeight: FontWeight.w200,
-                  fontFamily: "ProximaNovaBold"),
-            ),
-            value: _value,
-          ))
+                    child: Text(
+                      _value,
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          color: const Color(0xFF000000),
+                          fontWeight: FontWeight.w200,
+                          fontFamily: "ProximaNovaBold"),
+                    ),
+                    value: _value,
+                  ))
               .toList(),
           onChanged: (value) {
             solution = value;
           },
         ));
+  }
+
+  Widget buildAddProject() {
+    return ListView(
+      children: <Widget>[
+        Container(
+            child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(16),
+              child: Text("Создание проекта"),
+            ),
+            _dropDownIdeas(["идея 1", "идея 2"], "Выберите идею"),
+            _dropDownIdeas(["решение 1", "решение 2"], "Выберите решение"),
+            _dropDownIdeas(["работник 1", "работник 2"], "Пригласить участника"),
+            _dropDownIdeas(["работник 1", "работник 2"], "Пригласить участника"),
+            _dropDownIdeas(["работник 1", "работник 2"], "Пригласить участника"),
+            _dropDownIdeas(["работник 1", "работник 2"], "Пригласить участника"),
+            _dropDownIdeas(["работник 1", "работник 2"], "Пригласить участника"),
+          ],
+        ))
+      ],
+    );
+  }
+
+  Widget _dropDownIdeas(data, String hint) {
+    String selected;
+
+    var list = List<String>.from(data);
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: DropdownButtonFormField<String>(
+        hint: Text(hint),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+        ),
+        value: selected,
+        items: list
+            .map((_value) => DropdownMenuItem(
+                  child: Text(
+                    _value,
+                    style: TextStyle(
+                        fontSize: 12.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: "ProximaNovaBold"),
+                  ),
+                  value: _value,
+                ))
+            .toList(),
+        onChanged: (value) {
+          selected = value;
+        },
+      ),
+    );
   }
 }
